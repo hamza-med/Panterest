dd4319a9930f8df9ea93aad4ec88f24638b35146
Hamza Mohamed <hamzaa.mohamed1998@gmail.com>
2021-07-18 13:24:12 +0100

    Setup PinsController and index Action,Add Psysh Bundle,pluralize twig function

---
+++ src/Controller/PinsController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\PinRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PinsController extends AbstractController
+{
+    #[Route("/", name: 'app_home')]
+    public function index(PinRepository $pinRepository): Response
+    {
+        return $this->render('pins/index.html.twig',["pins"=>$pinRepository->findAll()]);
+    }
+}
