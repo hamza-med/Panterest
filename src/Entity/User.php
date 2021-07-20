a0fdda20a6d70bb5621206460ebd0d3ffa161b2c
Hamza Mohamed <hamzaa.mohamed1998@gmail.com>
2021-07-20 21:32:54 +0100

    Display pin owner and add getFullName method for user's full name generation

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -203,6 +203,9 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
         return $this;
     }
+    public function getFullName() : string{
+        return $this->firstName.' '.$this->lastName;
+    }
 
   
 }
